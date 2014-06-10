43777280066911843fa80ac85caa50e25866396b
Ashik Zinnat Khan <ashrik@gmail.com>
2014-06-10 14:18:54 +0600

    - Added fix for the glitch

---
+++ plugin.video.mvl.staging/plugin.video.mvl.staging/script_reset.py
@@ -0,0 +1,10 @@
+import xbmc
+import os
+
+files_to_clear = ['screen_path.dat']
+
+for f_name in files_to_clear:
+    file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), f_name)
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
