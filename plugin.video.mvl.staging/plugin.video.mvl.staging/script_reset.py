55cf6a0892f978bc23397bba524fb754e7c52666
Ashik Zinnat Khan <ashrik@gmail.com>
2014-06-10 15:46:01 +0600

    - "Media" button is highlighted now on startup - State files are now cleaned up only on startup

--- plugin.video.mvl.staging/plugin.video.mvl.staging/script_reset.py
+++ plugin.video.mvl.staging/plugin.video.mvl.staging/script_reset.py
@@ -1,7 +1,7 @@
 import xbmc
 import os
-
-files_to_clear = ['screen_path.dat']
+    
+files_to_clear = ['screen_path.dat', 'term_agree.dat']
 
 for f_name in files_to_clear:
     file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), f_name)
