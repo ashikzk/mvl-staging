aa927933e14e128add8e41911b78b8a06b808751
Ashik Zinnat Khan <ashrik@gmail.com>
2014-06-16 17:17:33 +0600

    - Added changes to metadata crawler

--- plugin.video.mvl.staging/plugin.video.mvl.staging/test_crawl.py
+++ plugin.video.mvl.staging/plugin.video.mvl.staging/test_crawl.py
@@ -1,4 +1,5 @@
 
+
 def crawl_categories(id, page):
 
     try:
@@ -18,22 +19,15 @@ def crawl_categories(id, page):
         content = f.read()
         items = []
 
+        print url
+
         if content:
             jsonObj = json.loads(content)
             totalCats = len(jsonObj)
             plugin.log.info('total categories-->%s' % totalCats)
-            plugin.log.info(jsonObj)
-            #if jsonObj[0]['top_level_parent'] == jsonObj[0]['parent_id']:
-            #    is_search_category = True
+            #plugin.log.info(jsonObj)
 
             for categories in jsonObj:
-                try:    # The last item of Json only contains the one element in array with key as "ID" so causing the issue
-                    if categories['top_level_parent'] == categories['parent_id']:
-                        main_category_check = True
-
-                except:
-                    pass
-
 
                 #categories['id'] is -1 when more categories are present and next page option should be displayed
                 if categories['id'] == -1:
@@ -51,21 +45,11 @@ def crawl_categories(id, page):
                         if 'parent_title' in categories:
                             #this must be a TV Show Season list
                             mvl_meta = create_meta('tvshow', categories['parent_title'].encode('utf-8'), '', '')
-
                         else:
                             mvl_meta = create_meta('tvshow', categories['title'].encode('utf-8'), '', '')
 
-                        items += [{
-                                      'id':categories['id'],
-                                      'page':0,
-                                  }]
-
                     else:
-
-                        items += [{
-                                      'id':categories['id'],
-                                      'page':0,
-                                  }]
+                        crawl_categories(categories['id'], 0)
 
                 #inorder for the information to be displayed properly, corresponding labels should be added in skin
                 elif categories['is_playable'] == 'True':
@@ -91,25 +75,7 @@ def crawl_categories(id, page):
                         if 'series_name' in mvl_meta:
                             series_name = mvl_meta['series_name'].strip()
 
-                    plugin.log.info('>> meta data-> %s' % mvl_meta)
-                    thumbnail_url = ''
-
-                    items += [{
-                                'id': '-1'
-                              }]
-
-
-            #if main_category_check == True:
-            #    #adding A-Z listing option
-            #    items += [{
-            #                  'label': button_label,
-            #                  'path': plugin.url_for('azlisting', category=parent_id),
-            #                  'thumbnail': art(button_name.lower()+'.png'),
-            #                  'is_playable': False,
-            #                  'context_menu': [('','',)],
-            #                  'replace_context_menu': True
-            #              }]
-            #
+                    #plugin.log.info('>> meta data-> %s' % mvl_meta)
 
         return items
 
@@ -120,11 +86,9 @@ def crawl_categories(id, page):
 
 
 
+
+
 ret = crawl_categories(1, 0)
-
-for r in ret:
-    ret2 = crawl_categories(r['id'], r['page'])
-
-print ret
+#ret = crawl_categories(3, 0)
 
 sys_exit()
