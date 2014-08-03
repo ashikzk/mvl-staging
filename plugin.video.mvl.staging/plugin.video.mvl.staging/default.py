ff81de35672168d6b44dc121308e058cbea5112a
Ashik Zinnat Khan <ashrik@gmail.com>
2014-08-03 16:57:06 +0600

    - Updated file for auto mark_as_watched

--- plugin.video.mvl.staging/plugin.video.mvl.staging/default.py
+++ plugin.video.mvl.staging/plugin.video.mvl.staging/default.py
@@ -1390,8 +1390,8 @@ def play_video(url, resolved_url, title, video_type, meta):
                                         season='', episode='', year=meta['year'], watch_percent=0.9,
                                         watchedCallbackwithParams=WatchedCallbackwithParams)
             else:
-                player = playbackengine.Player(addon_id='plugin.video.mvl', video_type='tvshow', title=title,
-                                        season=meta['season'], episode=meta['episode'], year='', watch_percent=0.01,
+                player = playbackengine.Player(addon_id='plugin.video.mvl', video_type='episode', title=meta['title'], imdb_id =meta['imdb_id'],
+                                        season=meta['season'], episode=meta['episode'], year='', watch_percent=0.001,
                                         watchedCallbackwithParams=WatchedCallbackwithParams)
 
             #try:
