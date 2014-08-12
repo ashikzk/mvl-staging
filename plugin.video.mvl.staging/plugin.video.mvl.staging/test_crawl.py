71db0a73d7e52540ef346e140a96b3dfdd497099
Ashik Zinnat Khan <ashrik@gmail.com>
2014-08-12 18:51:21 +0600

    - Added new pre-cached db file

--- plugin.video.mvl.staging/plugin.video.mvl.staging/test_crawl.py
+++ plugin.video.mvl.staging/plugin.video.mvl.staging/test_crawl.py
@@ -96,7 +96,7 @@ def crawl_categories(id, page, az_key):
 section_id = 1
 page_no = 0
 
-#ret = crawl_categories(section_id, page_no)
+ret = crawl_categories(section_id, page_no, None)
 
 az_keys = ['%23', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
            'V', 'W', 'X', 'Y', 'Z']
